3b6d35b4cd8b677f6b4591fca8d977b68da07c2b
Whoami155 <78509736+Whoami155@users.noreply.github.com>
2026-04-05 02:40:25 +0530

    node.ino

--- node.ino
+++ node.ino
@@ -1,35 +1,3 @@
-#define BLYNK_TEMPLATE_ID "TMPL3q519xQ8X"
-#define BLYNK_TEMPLATE_NAME "CLASSROOM ENVIRONMENT"
-#define BLYNK_AUTH_TOKEN "************"
-
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-
-char ssid[] = "AndroidAP_2853";
-char pass[] = "qxq12346";
-
-void setup()
-{
-  Serial.begin(9600);
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-}
-
-void loop()
-{
-  Blynk.run();
-
-  if (Serial.available())
-  {
-    String data = Serial.readStringUntil('\n');
-
-    int temp, hum, air;
-    sscanf(data.c_str(), "%d,%d,%d", &temp, &hum, &air);
-
-    Blynk.virtualWrite(V0, temp);
-    Blynk.virtualWrite(V1, hum);
-    Blynk.virtualWrite(V2, air);
-  }
-}
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
@@ -37,30 +5,29 @@ void loop()
 const char* ssid = "AndroidAP_2853";
 const char* password = "qxq12346";
 
-// 🌐 BACKEND
+// 🌐 BACKEND URL
 String serverURL = "http://10.65.191.195:5000/data";
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
 
-  Serial.println("\nStarting NodeMCU...");
-
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
+  Serial.print("Connecting");
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("\nWiFi Connected!");
+  Serial.println("\nConnected!");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
 
   if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Reconnecting WiFi...");
+    Serial.println("Reconnecting...");
     WiFi.begin(ssid, password);
     delay(2000);
     return;
@@ -71,14 +38,21 @@ void loop() {
     String data = Serial.readStringUntil('\n');
     data.trim();
 
-    Serial.print("Raw Data: ");
+    Serial.print("Received: ");
     Serial.println(data);
 
     float temperature, humidity, air;
 
-    int parsed = sscanf(data.c_str(), "%f,%f,%f", &temperature, &humidity, &air);
+    int parsed = sscanf(data.c_str(), "%f,%f,%f",
+                        &temperature, &humidity, &air);
 
     if (parsed == 3) {
+
+      bool alert = false;
+
+      if (air > 400) {
+        alert = true;
+      }
 
       WiFiClient client;
       HTTPClient http;
@@ -86,21 +60,26 @@ void loop() {
       http.begin(client, serverURL);
       http.addHeader("Content-Type", "application/json");
 
+      // JSON data
       String json = "{";
       json += "\"temperature\":" + String(temperature) + ",";
       json += "\"humidity\":" + String(humidity) + ",";
-      json += "\"air\":" + String(air);
+      json += "\"air\":" + String(air) + ",";
+      json += "\"alert\":" + String(alert ? "true" : "false");
       json += "}";
 
-      int httpResponseCode = http.POST(json);
+      Serial.print("Sending: ");
+      Serial.println(json);
+
+      int code = http.POST(json);
 
       Serial.print("Response: ");
-      Serial.println(httpResponseCode);
+      Serial.println(code);
 
       http.end();
 
     } else {
-      Serial.println("Invalid Data Format!");
+      Serial.println("Invalid data format");
     }
   }
 }
